44f7f2bae85fb2ea6eeef905ef5a2e3ea694da32
Elijah <ilya.bobr.thj@gmail.com>
2018-02-20 19:31:46 +0300

    add chat scoket functions

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import * as socket from 'socket.io';
 import routes from './routes';
 
 import appLogger from './utils/logger';
+import { globalAgent } from 'https';
 
 // tslint:disable-next-line:no-var-requires
 const cors = require('@koa/cors');
@@ -46,4 +47,19 @@ const io = socket.listen(server);
 
 server.listen(process.env.PORT);
 
-io.on('connection', (sock: any) => appLogger.info('connected' + sock.id, sock));
+io.on('connection', sock => {
+  appLogger.info(`User ${sock.id} start session`);
+  sock.on('join_room', (payload: any) => {
+    sock.join(payload.roomId);
+    io.to(payload.roomId).emit('message', { message: `User ${payload.user} joined to stream` });
+  });
+
+  sock.on('room_message', (payload: any) => {
+    io.to(payload.roomId).emit('message', { message: payload.message, user: payload.user });
+  });
+
+  sock.on('leave_room', (payload: any) => {
+    sock.leave(payload.roomId);
+    io.to(payload.roomId).emit('message', { message: `User ${payload.user} leave stream` });
+  });
+});
